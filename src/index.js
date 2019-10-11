9503c2cb50c99fa26ef34e2b1d2a352bfbc982e3
Valiantsin Horbach <valentin.com2000gorbach@gmail.com>
2019-10-11 16:11:08 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  for(let i=0; i<str.length; i++) 
+  {
+    let j = 0;
+    while (!bracketsConfig[j].includes(str[i]))
+      j++;
+    if (bracketsConfig[j][0] == str[i] && bracketsConfig[j][1] == str[i + 1]) 
+    {
+      str=str.substr(0,i)+str.substr(i+2);
+      i-=2;
+      if (i < -1) i = -1;
+    }    
+  }
+  return str.length == 0;
 }
